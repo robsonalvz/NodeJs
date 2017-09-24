591cb1a395d80bfb8d7c205a6157188b5bfbe5ff
robsonalvz <robson.alves@dce.ufpb.br>
2017-09-23 21:43:13 -0300

    readme

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,13 +1,17 @@
 module.exports = function(application){
-        application.get('/noticias',function(request,response){
-        
+    application.get('/noticias',function(request,response){
         var connection = application.config.dbConnection();
         var noticiasModel = new application.app.models.NoticiasDAO(connection);
-        
         noticiasModel.getNoticias(function(error,result){
             response.render("noticias/noticias",{noticias : result});
         });
-        
+    });  
+    application.get('/noticia',function(request,response){
+        var connection = application.config.dbConnection();
+        var noticiasModel = new application.app.models.NoticiasDAO(connection);
+        noticiasModel.getNoticia(function(error,result){
+            response.render("noticias/noticia",{noticia : result});
+        });
     });
 };
 
